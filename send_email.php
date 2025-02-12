6494f6970c80fd20413c5d7a6619c9e36a888464
Phuong Nguyen <22102700@student.westernsydney.edu.au>
2025-02-12 11:37:41 +1100

    Add all the information and complete the website

---
+++ send_email.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect input data and sanitize
+    $name = htmlspecialchars($_POST["name"]);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $subject = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    // Your email where messages will be sent
+    $to = "phuongpkm3012@gmail.com"; 
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Email content
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message\n";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<script>alert('Message sent successfully!'); window.location.href='index.html';</script>";
+    } else {
+        echo "<script>alert('Failed to send message. Try again later.'); window.location.href='index.html';</script>";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
